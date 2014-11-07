2bf08ebf78a066eea4af0d45872207f058c3ff79
Bryan Jensen <bryanawjensen@gmail.com>
2014-11-07 12:09:04 -0500

    Got all cards working - 14k entries

--- hw14/populate.js
+++ hw14/populate.js
@@ -23,18 +23,30 @@ function resetDatabase() {
     });
 }
 
+function preprocessJSON(cardJsonData) {
+    console.log('Preprocessing data');
+    return Object.keys(cardJsonData).map(function eachCard(entry, i, array) {
+        return entry;
+    });
+}
+
 function loadJSON() {
-    return readFile('KTK.json').then(JSON.parse);
+    return readFile('AllCards.json').then(JSON.parse).then(preprocessJSON);
 }
 
 function saveToDatabase(cardJsonData) {
+    console.log('Read data, saving to database');
     return new Promise(function promiseSave(resolve, reject) {
         MongoClient.connect('mongodb://bawjensen:dummytest@ds049160.mongolab.com:49160/webapp', function handleResponse(err, db) {
-            db.collection('hw14').insert(cardJsonData.cards, function handleWipeResponse(err) {
-                if (err) { reject(); }
+            console.log('Opened connection to database, inserting');
+            db.collection('hw14').insert(cardJsonData, function handleWipeResponse(err) {
+                if (err) { reject(Error(err)); }
                 else { resolve(); }
             });
         });
+    }).catch(function handlePromiseError(err) {
+        console.error(err);
+        throw err;
     });
 }
 
